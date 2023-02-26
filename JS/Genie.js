bea1337455c670c7e5fe12203e38d1afab22494a
mhmd-bal <mohammad.balouza@gmail.com>
2023-02-26 11:41:16 +0200

    Added accordion animations

--- JS/Genie.js
+++ JS/Genie.js
@@ -2,15 +2,15 @@ const accordions = document.getElementsByClassName("Accordion");
 
 for(let i=0; i < accordions.length; i++) {
     accordions[i].addEventListener("click", function(){
-        console.log("HELLO")
+        
         accordions[i].classList.toggle("Active");
 
         const accordion_text = accordions[i].nextElementSibling;
 
-        if (accordion_text.style.display == "none"){
-            accordion_text.style.display = "block";
+        if (accordion_text.style.maxHeight){
+            accordion_text.style.maxHeight = null;
         }else{
-            accordion_text.style.display = "none";
+            accordion_text.style.maxHeight = accordion_text.scrollHeight + "px";
         }
     });
 }
