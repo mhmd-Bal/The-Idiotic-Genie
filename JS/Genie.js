60c198882213bcf1679105e370fc99ce11203f21
mhmd-bal <mohammad.balouza@gmail.com>
2023-02-26 12:22:59 +0200

    Added the merge function

--- JS/Genie.js
+++ JS/Genie.js
@@ -15,3 +15,35 @@ for(let i=0; i < accordions.length; i++) {
     });
 }
 
+
+function printSortedNumbers(){
+    let numbers = prompt("Please give me the 5 numbers separated by space");
+    const numbers_array = numbers.split(" ");
+    let sorted_numbers = [];
+
+}
+
+function mergeArray(left, right){
+    temp_array = [];
+    while (left.length && right.length){
+        if(left[0] < right[0]){
+            temp_array.push(left.shift());
+        }else{
+            temp_array.push(right.shift());
+        }
+    }
+    return [...temp_array, ...left, ...right];
+}
+
+function mergeSortArray(temp_array){
+    if (temp_array.length <= 1){
+        return temp_array;
+    }
+
+    let mid = Math.floor(temp_array /2);
+
+    let left = mergeSortArray(temp_array.slice(0,mid));
+    let right = mergeSortArray(temp_array.slice(mid));
+
+    return mergeArray(left,right);
+}
