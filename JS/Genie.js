2aa0f2b8a54240149ff5ecec711f77732464fbcc
mhmd-bal <mohammad.balouza@gmail.com>
2023-02-26 12:58:56 +0200

    Cleaned the code

--- JS/Genie.js
+++ JS/Genie.js
@@ -1,4 +1,5 @@
 const accordions = document.getElementsByClassName("Accordion");
+const sorted_numbers_text = document.getElementById("Sorted_numbers");
 
 for(let i=0; i < accordions.length; i++) {
     accordions[i].addEventListener("click", function(){
@@ -19,12 +20,14 @@ for(let i=0; i < accordions.length; i++) {
 function printSortedNumbers(){
     let numbers = prompt("Please give me the 5 numbers separated by space");
     let numbers_string_array = numbers.split(" ");
-    console.log(numbers_string_array);
     let numbers_array = makeArrayNumbers(numbers_string_array);
     let sorted_array =  mergeSortArray(numbers_array);
-    const sorted_numbers_text = document.getElementById("Sorted_numbers");
-    for( let i=0; i < sorted_array.length; i++){
-        sorted_numbers_text.textContent +=  sorted_array[i] + " ";
+    printArrayInParagraph(sorted_numbers_text, sorted_array);
+}
+
+function printArrayInParagraph(temp_paragraph, temp_array){
+    for( let i=0; i < temp_array.length; i++){
+        temp_paragraph.textContent +=  temp_array[i] + " ";
     }
 }
 
@@ -44,12 +47,9 @@ function mergeSortArray(temp_array){
     if (temp_array.length <= 1){
         return temp_array;
     }
-
     let mid = Math.floor(temp_array.length/2);
-
     let left = mergeSortArray(temp_array.slice(0,mid));
     let right = mergeSortArray(temp_array.slice(mid));
-
     return mergeArray(left,right);
 }
 
@@ -58,6 +58,5 @@ function makeArrayNumbers(string_array){
     for (let i=0; i < string_array.length; i++){
         numbers_array.push(Number(string_array[i]));
     }
-    console.log(numbers_array);
     return numbers_array;
 }
